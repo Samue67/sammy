6cdfabcc74e0fb10d5bc7a3a44b8b55ddbf03e34
Samue67 <okeolajuwon0@gmail.com>
2024-07-02 00:12:33 +0100

    Add files via upload

---
+++ olajuwon's web/script.js
@@ -0,0 +1,9 @@
+// Add Javascript code here for interactivity (e.g., form validation, dynamic content loading)
+// Example: Handle form submission
+document.getElementById('contactForm').addEventListener( 'submit', Function(event)
+{
+    event.preventDefault();
+    // perform form validation and submission 
+    // Display success or error message
+    alert('fake Form submitted successfully!');
+});```
